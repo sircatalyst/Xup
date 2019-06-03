c269ee4dd2679ba1c5b8014dba3b9e553504f4fa
sircatalyst <temibami@gmail.com>
2019-06-03 18:50:58 +0100

    create meal routes

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import mealRoutes from "./routes/meal.route"
 let app = express();
 app.use(bodyParser.json());
 
@@ -9,6 +10,8 @@ let port = 1111;
 app.get('/', (req, res) => {
     return res.send("it's fine!")
 });
+
+app.use('/api/v1/meals', mealRoutes)
 
 app.listen(port, () => {
     console.log(`server running on ${port}`)
--- api/services/meal.service.js
+++ api/services/meal.service.js
@@ -3,26 +3,27 @@ import Meal from "../models/meal.model"
 
 const MealService = {
     fetchAllMeals() {
-        const validMeals =  dummyData.meals.app((meal) => {
+        const validMeals =  dummyData.meals.map((meal) => {
             const newMeal = new Meal();
             newMeal.id = meal.id;
             newMeal.name = meal.name;
             newMeal.size = meal.size;
             newMeal.price = meal.price;
+            return newMeal;
         });
         return validMeals;
  
     },
     addMeal(meal) {
         const mealLength = dummyData.meals.length;
-        const lastId = dummyData[meals.length - 1].id;
+        const lastId = dummyData.meals[mealLength - 1].id;
         const newId = lastId + 1;
         meal.id = newId;
         dummyData.meals.push(meal);
         return meal;
     },
     getAMeal(id){
-        const meal = dummyData.meals.find(meal => meal.id = id);
+        const meal = dummyData.meals.find(meal => meal.id == id);
         return meal || {};
     }
 }
