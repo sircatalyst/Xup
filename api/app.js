71e761ec82870ce7d0189edb432cafe19678ea46
sircatalyst <temibami@gmail.com>
2019-06-03 18:18:08 +0100

    commit fixed

--- api/app.js
+++ api/app.js
@@ -1,12 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 let app = express();
 app.use(bodyParser.json());
 
 let port = 1111;
-
 
 app.get('/', (req, res) => {
     return res.send("it's fine!")
