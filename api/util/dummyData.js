be6a527276d032aa70148276756bcebdaf1c332a
sircatalyst <temibami@gmail.com>
2019-06-03 16:24:49 +0100

    create meal service

---
+++ api/util/dummyData.js
@@ -0,0 +1,38 @@
+export default {
+    meals: [
+        {
+            id: 1,
+            name: "beans",
+            size: "medium",
+            price: "300"
+        },
+
+        {
+            id: 2,
+            name: "Fried Rice",
+            size: "medium",
+            price: "500"
+        },
+
+        {
+            id: 3,
+            name: "Jollof Rice",
+            size: "large",
+            price: "400"
+        },
+
+        {
+            id: 4,
+            name: "Eba",
+            size: "small",
+            price: "200"
+        },
+
+        {
+            id: 5,
+            name: "Amala",
+            size: "large",
+            price: "600"
+        }
+    ]
+}
